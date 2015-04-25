e4d116e84dfc8e3a80dcf1cf55b75969ed772727
xinshangshangxin <xinshangshangxin@gmail.com>
2015-04-25 14:16:41 +0800

    为静态版本提供接口

--- dist/routes/routes.js
+++ dist/routes/routes.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var httpServer = require('../models/httpServer');
 
+// 为静态版本提供接口
+var httpServerStatic = require('../models/httpServerStatic');
 
 module.exports = function(app) {
     app.get('/', getIndex);
@@ -9,6 +11,15 @@ module.exports = function(app) {
     app.get('/getsonginfo', httpServer.getSongInfo);
     app.get('/serverget', httpServer.serverGet);
     app.get('/getsongsbyids', httpServer.getSongsByIds);
+
+
+    // 为静态版本提供接口
+    app.get('/node/getchannellist', httpServerStatic.getchannellist);
+    app.get('/node/getsonglink', httpServerStatic.getSongLinks);
+    app.get('/node/getsonginfo', httpServerStatic.getSongInfo);
+    app.get('/node/serverget', httpServerStatic.serverGet);
+    app.get('/node/serverjson', httpServerStatic.serverJson);
+    app.get('/node/getsongsbyids', httpServerStatic.getSongsByIds);
 };
 
 function getIndex(req, res) {
